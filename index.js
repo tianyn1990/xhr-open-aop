0ef023c6bb7bd33dbb1b82cfbf71709de862ae46
tianyn1990 <tianyn1990@users.noreply.github.com>
2019-11-15 15:45:22 +0800

    add CommonJS version

---
+++ index.js
@@ -0,0 +1,28 @@
+function isFunc(fn) {
+    return fn && typeof fn === 'function';
+}
+function addXMLRequestOpenAOP({before, after} = {}) {
+    const _open = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function open(...args) {
+        const data = {
+            href: location.href,
+            method: args[0],
+            url: args[1],
+            async: args[2]
+        };
+        try {
+            if(isFunc(before) && before(this, data)) {
+                // 可中断
+                return;
+            }
+        } catch(e) {/**/}
+
+        isFunc(_open) && _open.apply(this, args);
+
+        try {
+            isFunc(after) && after(this, data);
+        } catch(e) {/**/}
+    };
+}
+
+module.exports = addXMLRequestOpenAOP;
